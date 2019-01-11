35f8dc33b4d4ddf8a8f56a1bb12c455db8495f6f
Alan Somers <asomers@gmail.com>
2019-01-11 13:18:06 -0700

    More integration tests for mockall_derive

--- mockall_derive/tests/test.rs
+++ mockall_derive/tests/test.rs
@@ -6,26 +6,6 @@ use std::default::Default;
 
 #[test]
 #[allow(unused)]
-fn simple_struct() {
-    #[mock]
-    struct SimpleStruct {
-        x: i16
-    }
-    #[mock]
-    impl SimpleStruct {
-        fn foo(&self, _x: u32) -> i64 {
-            42
-        }
-    }
-
-    let mut mock = MockSimpleStruct::default();
-    mock.expect_foo()
-        .returning(|x| i64::from(x) + 1);
-    assert_eq!(5, mock.foo(4));
-}
-
-#[test]
-#[allow(unused)]
 fn generic_struct() {
     #[mock]
     struct GenericStruct<'a, T, V> {
@@ -40,6 +20,131 @@ fn generic_struct() {
     }
 
     let mut mock = MockGenericStruct::<'static, u8, i8>::default();
+    mock.expect_foo()
+        .returning(|x| i64::from(x) + 1);
+    assert_eq!(5, mock.foo(4));
+}
+
+#[test]
+#[allow(unused)]
+fn impl_trait() {
+    trait Foo {
+        fn foo(&self, x: u32) -> i64;
+    }
+
+    #[mock]
+    struct SomeStruct {}
+
+    #[mock]
+    impl Foo for SomeStruct {
+        fn foo(&self, _x: u32) -> i64 {
+            42
+        }
+    }
+
+    let mut mock = MockSomeStruct::default();
+    mock.expect_foo()
+        .returning(|x| i64::from(x) + 1);
+    assert_eq!(5, mock.foo(4));
+}
+
+#[test]
+#[allow(unused)]
+fn method_self_by_value() {
+    #[mock]
+    struct MethodByValue {}
+
+    #[mock]
+    impl MethodByValue {
+        fn foo(self, _x: u32) -> i64 {
+            42
+        }
+    }
+
+    let mut mock = MockMethodByValue::default();
+    mock.expect_foo()
+        .returning(|x| i64::from(x) + 1);
+    assert_eq!(5, mock.foo(4));
+}
+
+#[test]
+#[allow(unused)]
+fn pub_crate_struct() {
+    #[mock]
+    pub(crate) struct PubStruct {
+        x: i16
+    }
+    #[mock]
+    impl PubStruct {
+        pub(crate) fn foo(&self, _x: u32) -> i64 {
+            42
+        }
+    }
+
+    let mut mock = MockPubStruct::default();
+    mock.expect_foo()
+        .returning(|x| i64::from(x) + 1);
+    assert_eq!(5, mock.foo(4));
+}
+
+#[test]
+#[allow(unused)]
+fn pub_super_struct() {
+    mod m {
+        use super::*;
+        #[mock]
+        pub(super) struct PubStruct {
+            x: i16
+        }
+        #[mock]
+        impl PubStruct {
+            pub(super) fn foo(&self, _x: u32) -> i64 {
+                42
+            }
+        }
+    }
+
+    let mut mock = m::MockPubStruct::default();
+    mock.expect_foo()
+        .returning(|x| i64::from(x) + 1);
+    assert_eq!(5, mock.foo(4));
+}
+
+#[test]
+#[allow(unused)]
+fn pub_struct() {
+    #[mock]
+    pub struct PubStruct {
+        x: i16
+    }
+    #[mock]
+    impl PubStruct {
+        pub fn foo(&self, _x: u32) -> i64 {
+            42
+        }
+    }
+
+    let mut mock = MockPubStruct::default();
+    mock.expect_foo()
+        .returning(|x| i64::from(x) + 1);
+    assert_eq!(5, mock.foo(4));
+}
+
+#[test]
+#[allow(unused)]
+fn simple_struct() {
+    #[mock]
+    struct SimpleStruct {
+        x: i16
+    }
+    #[mock]
+    impl SimpleStruct {
+        fn foo(&self, _x: u32) -> i64 {
+            42
+        }
+    }
+
+    let mut mock = MockSimpleStruct::default();
     mock.expect_foo()
         .returning(|x| i64::from(x) + 1);
     assert_eq!(5, mock.foo(4));
