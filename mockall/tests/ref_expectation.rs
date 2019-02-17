0be637827043facd345600bf1494817b46b6d110
Alan Somers <asomers@gmail.com>
2019-02-17 13:54:53 -0700

    Fixup to "Add ref_expectation"

--- mockall/tests/ref_expectation.rs
+++ mockall/tests/ref_expectation.rs
@@ -4,7 +4,11 @@ use mockall::*;
 
 #[test]
 fn match_eq_ok() {
-    let mut e = RefExpectation::<(i32), i32>::default();
+    ref_expectation!{
+        FooExpectation, __foo_priv, i32,
+        [i32], [&i], [i], [p], [i32]
+    }
+    let mut e = FooExpectation::default();
     e.return_const(99i32);
     e.with(predicate::eq(5));
     e.call(5);
@@ -13,7 +17,11 @@ fn match_eq_ok() {
 #[test]
 #[should_panic]
 fn match_eq_fail() {
-    let mut e = RefExpectation::<(i32), i32>::default();
+    ref_expectation!{
+        FooExpectation, __foo_priv, i32,
+        [i32], [&i], [i], [p], [i32]
+    }
+    let mut e = FooExpectation::default();
     e.return_const(99i32);
     e.with(predicate::eq(4));
     e.call(5);
@@ -21,7 +29,11 @@ fn match_eq_fail() {
 
 #[test]
 fn never_ok() {
-    let mut e = RefExpectation::<(), ()>::default();
+    ref_expectation!{
+        FooExpectation, __foo_priv, (),
+        [], [], [], [], []
+    }
+    let mut e = FooExpectation::default();
     e.return_const(());
     e.never();
 }
@@ -29,138 +41,186 @@ fn never_ok() {
 #[test]
 #[should_panic(expected = "Expectation should not have been called")]
 fn never_fail() {
-    let mut e = RefExpectation::<(), ()>::default();
+    ref_expectation!{
+        FooExpectation, __foo_priv, (),
+        [], [], [], [], []
+    }
+    let mut e = FooExpectation::default();
     e.return_const(());
     e.never();
-    e.call(());
+    e.call();
 }
 
 #[test]
 #[should_panic(expected = "Method sequence violation")]
 fn sequence_fail() {
+    ref_expectation!{
+        FooExpectation, __foo_priv, (),
+        [], [], [], [], []
+    }
+    let mut e0 = FooExpectation::default();
     let mut seq = Sequence::new();
-    let mut e0 = RefExpectation::<(), ()>::default();
     e0.return_const(());
     e0.times(1);
     e0.in_sequence(&mut seq);
 
-    let mut e1 = RefExpectation::<(), ()>::default();
+    let mut e1 = FooExpectation::default();
     e1.return_const(());
     e1.times(1);
     e1.in_sequence(&mut seq);
 
-    e1.call(());
-    e0.call(());
+    e1.call();
+    e0.call();
 }
 
 #[test]
 fn sequence_ok() {
+    ref_expectation!{
+        FooExpectation, __foo_priv, (),
+        [], [], [], [], []
+    }
+    let mut e0 = FooExpectation::default();
     let mut seq = Sequence::new();
-    let mut e0 = RefExpectation::<(), ()>::default();
     e0.return_const(());
     e0.times(1);
     e0.in_sequence(&mut seq);
 
-    let mut e1 = RefExpectation::<(), ()>::default();
+    let mut e1 = FooExpectation::default();
     e1.return_const(());
     e1.times(1);
     e1.in_sequence(&mut seq);
 
-    e0.call(());
-    e1.call(());
+    e0.call();
+    e1.call();
 }
 
 #[test]
 fn return_reference() {
-    let mut e = RefExpectation::<(), i32>::default();
+    ref_expectation!{
+        FooExpectation, __foo_priv, i32,
+        [], [], [], [], []
+    }
+    let mut e = FooExpectation::default();
     e.return_const(5i32);
-    assert_eq!(5i32, *e.call(()));
+    assert_eq!(5i32, *e.call());
 }
 
 #[test]
 fn return_str() {
     // This Expectation can be used for a method that returns &str
-    let mut e = RefExpectation::<(), String>::default();
+    ref_expectation!{
+        FooExpectation, __foo_priv, String,
+        [], [], [], [], []
+    }
+    let mut e = FooExpectation::default();
     e.return_const("abcd".to_owned());
-    assert_eq!("abcd", e.call(()));
+    assert_eq!("abcd", e.call());
 }
 
 #[test]
 fn times_any() {
-    let mut e = RefExpectation::<(), ()>::default();
+    ref_expectation!{
+        FooExpectation, __foo_priv, (),
+        [], [], [], [], []
+    }
+    let mut e = FooExpectation::default();
     e.return_const(());
     e.times(1);
     e.times_any();
-    e.call(());
-    e.call(());
+    e.call();
+    e.call();
 }
 
 #[test]
 fn times_ok() {
-    let mut e = RefExpectation::<(), ()>::default();
+    ref_expectation!{
+        FooExpectation, __foo_priv, (),
+        [], [], [], [], []
+    }
+    let mut e = FooExpectation::default();
     e.return_const(());
     e.times(2);
-    e.call(());
-    e.call(());
+    e.call();
+    e.call();
 }
 
 #[test]
 #[should_panic(expected = "Expectation called fewer than 2 times")]
 fn times_too_few() {
-    let mut e = RefExpectation::<(), ()>::default();
+    ref_expectation!{
+        FooExpectation, __foo_priv, (),
+        [], [], [], [], []
+    }
+    let mut e = FooExpectation::default();
     e.return_const(());
     e.times(2);
-    e.call(());
+    e.call();
 }
 
 #[test]
 #[should_panic(expected = "Expectation called more than 2 times")]
 fn times_too_many() {
-    let mut e = RefExpectation::<(), ()>::default();
+    ref_expectation!{
+        FooExpectation, __foo_priv, (),
+        [], [], [], [], []
+    }
+    let mut e = FooExpectation::default();
     e.return_const(());
     e.times(2);
-    e.call(());
-    e.call(());
-    e.call(());
+    e.call();
+    e.call();
+    e.call();
     // Verify that we panic quickly and don't reach code below this point.
     panic!("Shouldn't get here!");
 }
 
 #[test]
 fn times_range_ok() {
-    let mut e0 = RefExpectation::<(), ()>::default();
+    ref_expectation!{
+        FooExpectation, __foo_priv, (),
+        [], [], [], [], []
+    }
+    let mut e0 = FooExpectation::default();
     e0.return_const(());
     e0.times_range(2..4);
-    e0.call(());
-    e0.call(());
+    e0.call();
+    e0.call();
 
-    let mut e1 = RefExpectation::<(), ()>::default();
+    let mut e1 = FooExpectation::default();
     e1.return_const(());
     e1.times_range(2..4);
-    e1.call(());
-    e1.call(());
-    e1.call(());
+    e1.call();
+    e1.call();
+    e1.call();
 }
 
 #[test]
 #[should_panic(expected = "Expectation called fewer than 2 times")]
 fn times_range_too_few() {
-    let mut e = RefExpectation::<(), ()>::default();
+    ref_expectation!{
+        FooExpectation, __foo_priv, (),
+        [], [], [], [], []
+    }
+    let mut e = FooExpectation::default();
     e.return_const(());
     e.times_range(2..4);
-    e.call(());
+    e.call();
 }
 
 #[test]
 #[should_panic(expected = "Expectation called more than 3 times")]
 fn times_range_too_many() {
-    let mut e = RefExpectation::<(), ()>::default();
+    ref_expectation!{
+        FooExpectation, __foo_priv, (),
+        [], [], [], [], []
+    }
+    let mut e = FooExpectation::default();
     e.return_const(());
     e.times_range(2..4);
-    e.call(());
-    e.call(());
-    e.call(());
-    e.call(());
+    e.call();
+    e.call();
+    e.call();
+    e.call();
     // Verify that we panic quickly and don't reach code below this point.
     panic!("Shouldn't get here!");
 }
