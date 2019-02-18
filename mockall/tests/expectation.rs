35f7213832a32ced0bbdd70328dd4387a2b11dfb
Alan Somers <asomers@gmail.com>
2019-02-17 18:26:03 -0700

    Combine tests/omnimock with tests/expectation

--- mockall/tests/expectation.rs
+++ mockall/tests/expectation.rs
@@ -42,16 +42,84 @@ fn match_fn_fail() {
     e.call(5);
 }
 
-//#[test]
-//fn match_pointer_args() {
-    //let mut e = Expectation::<(*const u32), ()>::default();
-    //e.returning(|_| ());
-    //unsafe {
-        //e.withf_unsafe(|x: &*const u32| **x == 6);
-    //}
-    //let x = 6u32;
-    //e.call(&x as *const u32);
-//}
+mod reference_argument {
+    use super::*;
+
+    mockall::expectation!{FooExpectation, __foo_priv, u32,
+        [&u32], [&i0], [i0], [p0], [u32]}
+
+    #[test]
+    fn t() {
+        let mut e = FooExpectation::default();
+        e.with(predicate::eq(42u32))
+            .returning(|x| *x);
+        let x = 42u32;
+        assert_eq!(42u32, e.call(&x));
+    }
+
+    #[test]
+    fn match_fn() {
+        let mut e = FooExpectation::default();
+        e.withf(|x| *x == 42)
+            .returning(|x| *x);
+        let x = 42u32;
+        assert_eq!(42u32, e.call(&x));
+    }
+}
+
+mod ref_and_nonref_arguments {
+    use super::*;
+
+    mockall::expectation!{FooExpectation, __foo_priv, i32,
+        [i32, &u16],
+        [&i0, i1],
+        [i0, i1],
+        [p0, p1],
+        [i32, u16]
+    }
+
+    #[test]
+    fn t() {
+        let mut e = FooExpectation::default();
+        e.with(predicate::eq(42), predicate::eq(1))
+            .returning(|x, y| x + i32::from(*y));
+        let x = 42i32;
+        let y = 1u16;
+        assert_eq!(43i32, e.call(x, &y));
+    }
+
+    #[test]
+    fn match_fn() {
+        let mut e = FooExpectation::default();
+        e.withf(|x, y| *x == i32::from(*y))
+            .returning(|x, y| x + i32::from(*y));
+        let x = 42i32;
+        let y = 42u16;
+        assert_eq!(84i32, e.call(x, &y));
+    }
+}
+
+mod reference_arguments {
+    use super::*;
+
+    mockall::expectation!{FooExpectation, __foo_priv, u32,
+        [&u32, &u16],
+        [i0, i1],
+        [i0, i1],
+        [p0, p1],
+        [u32, u16]
+    }
+
+    #[test]
+    fn t() {
+        let mut e = FooExpectation::default();
+        e.with(predicate::eq(42), predicate::eq(1))
+            .returning(|x, y| *x + u32::from(*y));
+        let x = 42u32;
+        let y = 1u16;
+        assert_eq!(43u32, e.call(&x, &y));
+    }
+}
 
 /// A MockObject with a method that takes &mut self like:
 /// fn foo(&mut self, x: i32) -> u32
