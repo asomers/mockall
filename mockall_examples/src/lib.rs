298a0757122dd0930697f98fd854ca772016f784
Alan Somers <asomers@gmail.com>
2019-04-20 22:22:39 -0600

    More examples

--- mockall_examples/src/lib.rs
+++ mockall_examples/src/lib.rs
@@ -17,4 +17,23 @@ use mockall::*;
 pub trait Foo {
     /// A method with a `'static` return type
     fn foo(&self, x: i32, y: i16) -> i32;
+
+    /// A method returning a reference
+    fn bar(&self, x: i32) -> &i32;
+
+    /// A method returning a mutable reference
+    fn baz(&mut self, x: i32) -> &mut i32;
+
+    /// A method returning a `'static` reference
+    fn bean(&self) -> &'static i32;
+
+    /// A static method
+    fn bang(x: i32) -> i32;
+}
+
+#[cfg(all(feature = "nightly-docs", rustdoc))]
+#[automock(mod mock_ffi;)]
+extern "C" {
+    /// A foreign "C" function
+    pub fn ffi_func();
 }
