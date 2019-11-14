0bd4780c828cca2dd106a7175607b309c939c701
Alan Somers <asomers@gmail.com>
2019-11-13 22:47:46 -0700

    Fix mocking methods when a custom Result type is in-scope
    There was a hygiene violation in the proc macro.  It would cause
    automock and mock to fail anytime a custom Result type was in scope,
    even if the mocked method didn't try to use it.
    
    Fixes #73

---
+++ mockall/tests/automock_custom_result.rs
@@ -0,0 +1,41 @@
+// vim: tw=80
+//! It should be possible to use a custom Result type in the signature of a
+//! mocked method.  Regression test for
+//! https://github.com/asomers/mockall/issues/73
+
+use mockall::*;
+
+pub type Result<T> = std::result::Result<T, String>;
+
+pub struct MyStruct {}
+
+#[automock]
+impl MyStruct {
+    pub fn ret_static(&self) -> Result<i32> { unimplemented!() }
+    pub fn ret_ref(&self) -> &Result<i32> { unimplemented!() }
+    pub fn ret_refmut(&mut self) -> &mut Result<i32> { unimplemented!() }
+}
+
+#[test]
+fn ret_ref() {
+    let mut s = MockMyStruct::new();
+    s.expect_ret_ref()
+        .return_const(Ok(42));
+    assert_eq!(Ok(42), *s.ret_ref());
+}
+
+#[test]
+fn ret_ref_mut() {
+    let mut s = MockMyStruct::new();
+    s.expect_ret_refmut()
+        .return_var(Ok(42));
+    assert_eq!(Ok(42), *s.ret_refmut());
+}
+
+#[test]
+fn ret_static() {
+    let mut s = MockMyStruct::new();
+    s.expect_ret_static()
+        .return_const(Ok(42));
+    assert_eq!(Ok(42), s.ret_static());
+}
