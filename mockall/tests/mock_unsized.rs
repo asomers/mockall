1d9c55c5bc6212d944777901aea686c6d2910917
Alan Somers <asomers@gmail.com>
2019-11-20 21:48:49 -0700

    Update predicates to 1.0.2
    This fixes using `predicate::{always, never}` with `?Sized` types.
    
    Fixes #78

---
+++ mockall/tests/mock_unsized.rs
@@ -0,0 +1,51 @@
+// vim: tw=80
+//! All types of predicate should work for methods with unsized arguments
+
+use mockall::*;
+
+mock! {
+    Foo {
+        fn foo(&self, arg: &str);
+    }
+}
+
+#[test]
+fn with_always() {
+    let mut foo = MockFoo::new();
+    foo.expect_foo()
+        .with(predicate::always())
+        .return_const(());
+
+    foo.foo("xxx");
+}
+
+#[test]
+fn with_eq() {
+    let mut foo = MockFoo::new();
+    foo.expect_foo()
+        .with(predicate::eq("xxx"))
+        .return_const(());
+
+    foo.foo("xxx");
+}
+
+#[test]
+#[should_panic(expected = "MockFoo::foo: No matching expectation found")]
+fn with_never() {
+    let mut foo = MockFoo::new();
+    foo.expect_foo()
+        .with(predicate::never())
+        .return_const(());
+
+    foo.foo("xxx");
+}
+
+#[test]
+fn withf() {
+    let mut foo = MockFoo::new();
+    foo.expect_foo()
+        .withf(|a| a == "xxx")
+        .return_const(());
+
+    foo.foo("xxx");
+}
