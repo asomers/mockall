d974265399c603c4e3843381aed67b13751b9cb9
Alan Somers <asomers@gmail.com>
2019-12-01 09:22:29 -0700

    Fix regression mocking generic methods with type bounds
    generic struct with type bounds on their struct's generic parameters
    that also have generic methods are probably affected.
    
    Fixes #88

---
+++ mockall/tests/automock_generic_method_with_bounds.rs
@@ -0,0 +1,22 @@
+// vim: tw=80
+//! generic methods with bounds on their generic parameters
+
+use mockall::*;
+use std::fmt::Debug;
+
+struct X<T: Debug>(T);
+
+#[automock]
+trait Foo {
+    fn foo<T: Debug + 'static>(&self, x: X<T>);
+}
+
+#[test]
+fn withf() {
+    let mut mock = MockFoo::new();
+    mock.expect_foo::<u32>()
+        .withf(|x| x.0 == 42u32)
+        .return_const(());
+
+    mock.foo(X(42u32));
+}
