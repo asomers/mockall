asomers/mockall

commit b2d762e578562bdc3dba1bcc2b9368cce310263a

mock generic methods returning non-static values
It's now possible to mock generic methods whose return values' lifetimes are
chosen by the caller.  This is especially useful for methods that return
std::future::Future.

There are presently two restrictions:
1) static methods and bare functions of this type are not supported
2) The expectations' return values must be 'static.  That can often be
   worked-around via an unsafe lifetime transmute.

Hopefully both of those restrictions will be lifted in time.

Fixes #76

mockall_derive/src/lib.rs
@@ -50,7 +50,6 @@ struct MethodTypes {
     /// Method to call when invoking the expectation
     call: Ident,
     /// Expressions that should be used for Expectation::call's arguments
-    // TODO: can this be a Vec instead?
     call_exprs: Punctuated<TokenStream, Token![,]>,
     /// Method's argument list
     inputs: Punctuated<FnArg, Token![,]>,
@@ -791,10 +790,10 @@ fn method_types(sig: &Signature, generics: Option<&Generics>) -> MethodTypes {
     } else {
         sig.generics.clone()
     };
-    let (no_lt_g, _, _) = split_lifetimes(merged_generics, &sig.inputs,
-                                          &sig.output);
     let inputs = demutify(&sig.inputs);
-    let (_ig, tg, _wc) = no_lt_g.split_for_impl();
+    let (no_lt_g, _, rlg) = split_lifetimes(merged_generics.clone(),
+                                            &sig.inputs, &sig.output);
+    let with_ret_lt_g = merge_generics(&no_lt_g, &rlg);
     for fn_arg in expectation_inputs.iter() {
         match fn_arg {
             FnArg::Typed(_) => {},
@@ -847,12 +846,18 @@ fn method_types(sig: &Signature, generics: Option<&Generics>) -> MethodTypes {
     let expectations = parse2(
         quote!(#ident::#expectations_ident)
     ).unwrap();
+
+    // Replace any generic lifetimes of the Expectation's type with 'static
+    // TODO: in the future, only replace those lifetimes that _don't_ appear
+    // in the original trait's or struct's signature.
+    let expectation_g = staticize(&with_ret_lt_g);
+    let (_, expectation_tg, _) = expectation_g.split_for_impl();
+    let expect_ts = quote!(#ident::#expectation_ident #expectation_tg);
     let expect_obj = if is_expectation_generic {
-        parse2(quote!(#expectations)).unwrap()
+        parse2(quote!(#expectations))
     } else {
-        parse2(quote!(#expectations #tg)).unwrap()
-    };
-    let expect_ts = quote!(#ident::#expectation_ident #tg);
+        parse2(quote!(#expectations #expectation_tg))
+    }.unwrap();
     let expectation: Type = parse2(expect_ts).unwrap();
     let mut output = sig.output.clone();
     deimplify(&mut output);
@@ -860,6 +865,14 @@ fn method_types(sig: &Signature, generics: Option<&Generics>) -> MethodTypes {
     MethodTypes{is_static, is_expectation_generic, expectation,
                 expectation_generics, expectation_inputs, expectations, call,
                 expect_obj, call_exprs, inputs, output}
+}
+
+fn staticize(generics: &Generics) -> Generics {
+    let mut ret = generics.clone();
+    for lt in ret.lifetimes_mut() {
+        lt.lifetime = Lifetime::new("'static", Span::call_site());
+    };
+    ret
 }
 
 #[proc_macro]
@@ -1014,6 +1027,26 @@ mod method_types{
         assert_eq!(mt.call, "call");
         assert_eq!(mt.inputs, inputs);
         assert_eq!(mt.output, parse2(quote!(-> u32)).unwrap());
+    }
+
+    #[test]
+    fn generic_method_returning_nonstatic() {
+        let tim: TraitItemMethod = parse2(quote!(
+            fn foo<'a>(&self) -> X<'a>;
+        )).unwrap();
+        let mt = method_types(&tim.sig, None);
+        assert!(!mt.is_static);
+        assert!(!mt.is_expectation_generic);
+        assert_eq!(mt.expectation,
+                   parse2(quote!(foo::Expectation<'static>)).unwrap());
+        assert_eq!(mt.expectation_generics,
+                   parse2(quote!(<'a>)).unwrap());
+        assert_eq!(mt.expectations,
+                   parse2(quote!(foo::Expectations)).unwrap());
+        assert_eq!(mt.expect_obj,
+                   parse2(quote!(foo::Expectations<'static>)).unwrap());
+        assert_eq!(mt.call, "call");
+        assert_eq!(mt.output, parse2(quote!(-> X<'a>)).unwrap());
     }
 
     #[test]

mockall/tests/mock_generic_method_returning_nonstatic.rs
@@ -1,0 +1,44 @@
+// vim: tw=80
+//! A generic method whose return value's lifetime is a generic parameter is a
+//! special case.  Mockall can only mock such methods if the expectation is
+//! 'static.
+//! https://github.com/asomers/mockall/issues/76
+
+use mockall::*;
+
+#[derive(Clone)]
+struct X<'a>(&'a u32);
+
+mock! {
+    Thing {
+        fn foo<'a>(&self) -> X<'a>;
+        // XXX static methods don't work yet.
+        // fn bar<'a>() -> X<'a>;
+    }
+}
+
+#[test]
+fn return_static() {
+    const D: u32 = 42;
+    let x = X(&D);
+    let mut thing = MockThing::new();
+    thing.expect_foo()
+        .return_const(x);
+
+    assert_eq!(42u32, *thing.foo().0);
+}
+
+// It isn't possible to safely set an expectation for a non-'static return value
+// (because the mock object doesn't have any lifetime parameters itself), but
+// unsafely setting such an expectation is a common use case.
+#[test]
+fn return_nonstatic() {
+    let d = 42u32;
+    let x = X(&d);
+    let xstatic: X<'static> = unsafe{ std::mem::transmute(x) };
+    let mut thing = MockThing::new();
+    thing.expect_foo()
+        .returning(move || xstatic.clone());
+
+    assert_eq!(42u32, *thing.foo().0);
+}

mockall_derive/src/automock.rs
@@ -409,13 +409,15 @@ fn mock_function(modname: &Ident, vis: &Visibility, sig: &Signature)
     let meth_vis = expectation_visibility(&vis, 1);
     let context_ident = format_ident!("{}_context", &ident);
     let expect_vis = expectation_visibility(&vis, 2);
+    let (_, tg, _) = generics.split_for_impl();
+    let expect_obj = parse2(quote!(Expectations #tg)).unwrap();
     let mut g = generics.clone();
     let lt = Lifetime::new("'guard", Span::call_site());
     let ltd = LifetimeDef::new(lt);
     g.params.push(GenericParam::Lifetime(ltd.clone()));
 
     let mut out = TokenStream::new();
-    Expectation::new(&TokenStream::new(), &inputs, None, generics,
+    Expectation::new(&TokenStream::new(), &inputs, &expect_obj, None, generics,
         &ident, &mod_ident, None, &sig.output, &expect_vis, 1)
         .to_tokens(&mut out);
     let no_match_msg = format!("{}::{}: No matching expectation found",

mockall_derive/src/expectation.rs
@@ -49,6 +49,8 @@ struct Common<'a> {
     /// Types of the method arguments
     argty: Vec<Type>,
     attrs: &'a TokenStream,
+    /// Type of the Expectations object, with generics
+    expect_obj: &'a Type,
     /// The expectation's generic types as a list of types
     fn_params: Punctuated<Ident, Token![,]>,
     /// Generics of the parent struct
@@ -195,7 +197,8 @@ impl<'a> Common<'a> {
 
     /// Common methods of the Expectations structs
     fn expectations_methods(&self) -> TokenStream {
-        let (ig, tg, wc) = self.egenerics.split_for_impl();
+        let generics = merge_generics(&self.egenerics, &self.rlifetimes);
+        let (ig, tg, wc) = generics.split_for_impl();
         let v = &self.vis;
         quote!(
             /// A collection of [`Expectation`](struct.Expectations.html)
@@ -385,7 +388,7 @@ impl<'a> Expectation<'a> {
                 idx
             }).collect::<Vec<_>>();
         let matcher_ts = quote!(
-            enum Matcher #ig #wc {
+            enum Matcher #tg #wc {
                 Always,
                 Func(Box<dyn #hrtb Fn(#refpredty) -> bool + Send>),
                 Pred(Box<(#preds)>),
@@ -574,6 +577,8 @@ impl<'a> Expectation<'a> {
     ///                       `#[cfg()]`
     /// * `args`            - Arguments for the mock method, which may be
     ///                       slightly different than on the original method.
+    /// * `expect_obj       - Type of Expectations object stored in the mock
+    ///                       structure, with generics fields
     /// * `struct_generics` - Generics of the parent struct, if any.
     /// * `meth_generics`   - Generics of the method being mocked
     /// * `mod_ident`       - Name of the expectaton's private module
@@ -585,6 +590,7 @@ impl<'a> Expectation<'a> {
     pub(crate) fn new(
         attrs: &'a TokenStream,
         args: &Punctuated<FnArg, Token![,]>,
+        expect_obj: &'a Type,
         struct_generics: Option<&'a Generics>,
         meth_generics: &'a Generics,
         meth_ident: &'a Ident,
@@ -671,6 +677,7 @@ impl<'a> Expectation<'a> {
             argnames,
             argty,
             attrs,
+            expect_obj,
             fn_params,
             struct_generics,
             meth_generics,
@@ -728,7 +735,9 @@ impl<'a> StaticExpectation<'a> {
         let argnames = &self.common.argnames;
         let argty = &self.common.argty;
         let ident_str = self.common().ident_str();
-        let (ig, tg, wc) = self.common.egenerics.split_for_impl();
+        let generics = merge_generics(&self.common.egenerics, &self.common.rlifetimes);
+        let (ig, tg, wc) = generics.split_for_impl();
+        let (_, common_tg, _) = self.common.egenerics.split_for_impl();
         let hrtb = self.common.hrtb();
         let lg = &self.common.alifetimes;
         let output = &self.common.output;
@@ -737,7 +746,7 @@ impl<'a> StaticExpectation<'a> {
             /// Expectation type for methods that return a `'static` type.
             /// This is the type returned by the `expect_*` methods.
             #v struct Expectation #ig #wc {
-                common: Common #tg,
+                common: Common #common_tg,
                 rfunc: Mutex<Rfunc #tg>,
             }
 
@@ -864,7 +873,9 @@ impl<'a> StaticExpectation<'a> {
     fn expectations_methods(&self) -> TokenStream {
         let argnames = &self.common.argnames;
         let argty = &self.common.argty;
-        let (ig, tg, wc) = self.common.egenerics.split_for_impl();
+        let generics = merge_generics(&self.common.egenerics,
+                                      &self.common.rlifetimes);
+        let (ig, tg, wc) = generics.split_for_impl();
         let lg = &self.common.alifetimes;
         let output = &self.common.output;
         let predexprs = &self.common.predexprs;
@@ -936,7 +947,9 @@ impl<'a> StaticExpectation<'a> {
     }
 
     fn rfunc(&self) -> TokenStream {
-        let (ig, tg, wc) = self.common.egenerics.split_for_impl();
+        let generics = merge_generics(&self.common.egenerics,
+                                      &self.common.rlifetimes);
+        let (ig, tg, wc) = generics.split_for_impl();
         let hrtb = self.common.hrtb();
         let lg = &self.common.alifetimes;
         let argnames = &self.common.argnames;
@@ -1023,6 +1036,7 @@ impl<'a> StaticExpectation<'a> {
 
         let argnames = &self.common.argnames;
         let argty = &self.common.argty;
+        let expect_obj = &self.common.expect_obj;
         let fn_params = &self.common.fn_params;
         let (_ig, tg, _wc) = self.common.egenerics.split_for_impl();
         let hrtb = self.common.hrtb();
@@ -1117,7 +1131,7 @@ impl<'a> StaticExpectation<'a> {
             quote!(
                 ::mockall::lazy_static! {
                     #v static ref EXPECTATIONS:
-                        ::std::sync::Mutex<Expectations> =
+                        ::std::sync::Mutex<#expect_obj> =
                         ::std::sync::Mutex::new(Expectations::new());
                 }
                 /// Like an [`&Expectation`](struct.Expectation.html) but
@@ -1133,7 +1147,8 @@ impl<'a> StaticExpectation<'a> {
                 // ExpectationGuard is only defined for expectations that return
                 // 'static return types.
                 #v struct ExpectationGuard #e_ig #e_wc {
-                    guard: MutexGuard<'__mockall_lt, Expectations #tg>,
+                    guard: MutexGuard<'__mockall_lt, #expect_obj>,
+                    //guard: MutexGuard<'__mockall_lt, Expectations #tg>,
                     i: usize
                 }
 

mockall_derive/src/mock.rs
@@ -389,6 +389,7 @@ fn gen_struct<T>(mock_ident: &syn::Ident,
         }
 
         Expectation::new(&attrs, &meth_types.expectation_inputs,
+                         &meth_types.expect_obj,
                          Some(&generics), &meth_types.expectation_generics,
                          meth_ident, meth_ident, Some(&mock_ident), output,
                          &expect_vis, 2).to_tokens(&mut mod_body);

commit 435245a8649dcd57e9415c5ef12d1b32fc997a47

Fix regression mocking "impl trait" for generic traits
The regression was probably introduced by PR #86

mockall_derive/src/lib.rs
@@ -446,7 +446,18 @@ fn find_lifetimes(ty: &Type) -> HashSet<Lifetime> {
             }
             ret
         },
-        _ => unimplemented!()
+        Type::ImplTrait(tit) => {
+            let mut ret = HashSet::default();
+            for tpb in tit.bounds.iter() {
+                ret.extend(find_lifetimes_in_tpb(tpb));
+            }
+            ret
+        },
+        _ => {
+            compile_error(ty.span(), "unsupported type in this context");
+            HashSet::default()
+        }
+
     }
 }
 
@@ -675,7 +686,6 @@ fn split_lifetimes(
         match arg {
             FnArg::Typed(pt) => {
                 alts.extend(find_lifetimes(pt.ty.as_ref()))
-                //alts.extend(find_lifetimes(pt.ty.as_ref(), args, rt))
             },
             _ => ()
         };

mockall/tests/mock_impl_generic_trait.rs
@@ -1,0 +1,21 @@
+// vim: tw=80
+
+use mockall::*;
+
+trait MyTrait<T> {}
+
+struct MyStruct<T>(T);
+impl<T> MyTrait<T> for MyStruct<T> {}
+
+mock!{
+    Foo {
+        fn foo<R: 'static>(&self) -> impl MyTrait<R>;
+    }
+}
+
+#[test]
+fn returning() {
+    let mut mock = MockFoo::new();
+    mock.expect_foo::<u32>().returning(|| Box::new(MyStruct(42u32)));
+    let _mto: Box<dyn MyTrait<u32>> = mock.foo();
+}
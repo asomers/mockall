435245a8649dcd57e9415c5ef12d1b32fc997a47
Alan Somers <asomers@gmail.com>
2019-12-05 21:46:29 -0700

    Fix regression mocking "impl trait" for generic traits
    The regression was probably introduced by PR #86

---
+++ mockall/tests/mock_impl_generic_trait.rs
@@ -0,0 +1,21 @@
+// vim: tw=80
+
+use mockall::*;
+
+trait MyTrait<T> {}
+
+struct MyStruct<T>(T);
+impl<T> MyTrait<T> for MyStruct<T> {}
+
+mock!{
+    Foo {
+        fn foo<R: 'static>(&self) -> impl MyTrait<R>;
+    }
+}
+
+#[test]
+fn returning() {
+    let mut mock = MockFoo::new();
+    mock.expect_foo::<u32>().returning(|| Box::new(MyStruct(42u32)));
+    let _mto: Box<dyn MyTrait<u32>> = mock.foo();
+}
