4d21b6f02616627635912fa94831986c6ac25c02
Alan Somers <asomers@gmail.com>
2020-05-19 20:42:03 -0600

    implement mock! for structs

--- mockall_derive/src/mockable_struct.rs
+++ mockall_derive/src/mockable_struct.rs
@@ -20,6 +20,24 @@ fn mockable_method(mut meth: ImplItemMethod) -> ImplItemMethod {
     meth
 }
 
+/// Converts a TraitItemMethod into an ImplItemMethod
+fn tim2iim(m: syn::TraitItemMethod, vis: &syn::Visibility)
+    -> syn::ImplItemMethod
+{
+    let empty_block = Block {
+        brace_token: token::Brace::default(),
+        stmts: Vec::new()
+    };
+    syn::ImplItemMethod{
+        attrs: m.attrs,
+        vis: vis.clone(),
+        defaultness: None,
+        sig: m.sig,
+        block: empty_block
+    }
+}
+
+
 pub(crate) struct MockableStruct {
     pub attrs: Vec<Attribute>,
     pub generics: Generics,
@@ -36,19 +54,14 @@ impl From<(Attrs, ItemTrait)> for MockableStruct {
         let trait_ = attrs.substitute_trait(&item_trait);
         let mut methods = Vec::new();
         let pub_token = Token![pub](Span::call_site());
+        let meth_vis = Visibility::Public(VisPublic{pub_token}); 
         let empty_block = Block {
             brace_token: token::Brace::default(),
             stmts: Vec::new()
         };
         for item in item_trait.items.into_iter() {
             if let TraitItem::Method(meth) = item {
-                let iim = ImplItemMethod {
-                    attrs: meth.attrs,
-                    block: empty_block.clone(),
-                    defaultness: None,
-                    sig: meth.sig,
-                    vis: Visibility::Public(VisPublic{pub_token})
-                };
+                let iim = tim2iim(meth, &meth_vis);
                 methods.push(mockable_method(iim));
             }
         }
@@ -99,6 +112,43 @@ impl From<ItemImpl> for MockableStruct {
 
 impl Parse for MockableStruct {
     fn parse(input: ParseStream) -> syn::parse::Result<Self> {
-        unimplemented!("7");
+        let attrs = input.call(syn::Attribute::parse_outer)?;
+        let vis: syn::Visibility = input.parse()?;
+        let original_name: syn::Ident = input.parse()?;
+        let mut generics: syn::Generics = input.parse()?;
+        let wc: Option<syn::WhereClause> = input.parse()?;
+        generics.where_clause = wc;
+
+        let impl_content;
+        let _brace_token = braced!(impl_content in input);
+        let mut methods = Vec::new();
+        while !impl_content.is_empty() {
+            let method: syn::TraitItem = impl_content.parse()?;
+            match method {
+                syn::TraitItem::Method(meth) => {
+                    methods.push(tim2iim(meth, &vis))
+                },
+                _ => {
+                    return Err(input.error("Unsupported in this context"));
+                }
+            }
+        }
+
+        //let mut traits = Vec::new();
+        //while !input.is_empty() {
+            //let trait_: syn::ItemTrait = input.parse()?;
+            //traits.push(trait_);
+        //}
+
+        Ok(
+            MockableStruct {
+                attrs: attrs,
+                vis,
+                name: gen_mock_ident(&original_name),
+                generics,
+                methods,
+                original_name
+            }
+        )
     }
 }
