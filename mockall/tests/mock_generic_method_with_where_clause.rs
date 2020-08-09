57e530479b2f7294651fe4679e6d4c1120eded9f
Alan Somers <asomers@gmail.com>
2020-08-08 20:59:16 -0600

    Fix generic methods returning references with where clauses

--- mockall/tests/mock_generic_method_with_where_clause.rs
+++ mockall/tests/mock_generic_method_with_where_clause.rs
@@ -7,6 +7,8 @@ mock! {
     Foo {
         fn foo<T>(&self, t: T) -> G<T> where T: Copy + 'static;
         fn bar<T>(&self, g: G<T>) -> T where T: Copy + 'static;
+        fn baz<T>(&self) -> &G<T> where T: Copy + 'static;
+        fn bean<T>(&mut self) -> &mut G<T> where T: Copy + 'static;
     }
 }
 
@@ -20,4 +22,21 @@ fn returning() {
     mock.expect_bar::<u32>()
         .returning(|g| g.t);
     assert_eq!(42u32, mock.bar(G{t: 42}));
+}
+
+#[test]
+fn return_const() {
+    let mut mock = MockFoo::new();
+    mock.expect_baz::<u32>()
+        .return_const(G{t: 42});
+    assert_eq!(42u32, mock.baz().t);
+}
+
+#[test]
+fn return_var() {
+    let mut mock = MockFoo::new();
+    mock.expect_bean::<u32>()
+        .return_var(G{t: 42});
+    mock.bean::<u32>().t += 1;
+    assert_eq!(43u32, mock.bean().t);
 }
