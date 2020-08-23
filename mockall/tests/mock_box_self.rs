c75e6592bea8bcc9d7730e018612706c23ac49ef
Alan Somers <asomers@gmail.com>
2020-08-23 12:21:37 -0600

    Allow mocking methods with complicated receivers like self: Box<Self>
    Fixes #150
    Fixes #109

---
+++ mockall/tests/mock_box_self.rs
@@ -0,0 +1,67 @@
+// vim: tw=80
+//! Methods that take receivers like Box<Self> instead of &self
+#![deny(warnings)]
+
+use mockall::*;
+use std::sync::Arc;
+use std::pin::Pin;
+use std::rc::Rc;
+
+mock! {
+    Foo {
+        fn foo(self: &Box<Self>);
+        fn baz(mut self: Box<Self>);
+        fn bar(self: Box<Self>);
+        fn bean(self: Arc<Self>);
+        fn booz(self: Pin<Box<Self>>);
+        fn blez(self: Rc<Self>);
+    }
+}
+
+#[test]
+fn arc() {
+    let mut mock = MockFoo::new();
+    mock.expect_bean()
+        .returning(|| ());
+    Arc::new(mock).bean();
+}
+
+#[test]
+fn pin() {
+    let mut mock = MockFoo::new();
+    mock.expect_booz()
+        .returning(|| ());
+    Pin::new(Box::new(mock)).booz();
+}
+
+#[test]
+fn rc() {
+    let mut mock = MockFoo::new();
+    mock.expect_blez()
+        .returning(|| ());
+    Rc::new(mock).blez();
+}
+
+#[test]
+fn ref_box() {
+    let mut mock = Box::new(MockFoo::new());
+    mock.expect_foo()
+        .returning(|| ());
+    mock.foo();
+}
+
+#[test]
+fn mutable() {
+    let mut mock = Box::new(MockFoo::new());
+    mock.expect_baz()
+        .returning(|| ());
+    mock.baz();
+}
+
+#[test]
+fn owned() {
+    let mut mock = Box::new(MockFoo::new());
+    mock.expect_bar()
+        .returning(|| ());
+    mock.bar();
+}
