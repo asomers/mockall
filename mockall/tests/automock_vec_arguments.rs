56644b9685345b063562dd8222b52379c57d3305
Piotr Maks <piotr.maks@gmail.com>
2020-09-03 12:59:58 +0200

    Mock method with `String` or `Vec<T> arguments

---
+++ mockall/tests/automock_vec_arguments.rs
@@ -0,0 +1,35 @@
+// vim: tw=80
+//! A method with `Vec<T>` arguments
+#![deny(warnings)]
+
+use mockall::*;
+
+#[automock]
+trait Foo {
+    fn foo<T: 'static>(&self, x: Vec<T>);
+}
+
+mod withf {
+    use super::*;
+
+    #[test]
+    #[should_panic(expected = "MockFoo::foo: No matching expectation found")]
+    fn fail() {
+        let mut mock = MockFoo::new();
+        mock.expect_foo::<i32>()
+            .withf(|sl| sl == &vec![1, 2, 3])
+            .return_const(());
+        let x = vec![1, 2, 3, 4];
+        mock.foo(x);
+    }
+
+    #[test]
+    fn ok() {
+        let mut mock = MockFoo::new();
+        mock.expect_foo::<i32>()
+            .withf(|sl| sl == &vec![1, 2, 3])
+            .return_const(());
+        let x = vec![1, 2, 3];
+        mock.foo(x);
+    }
+}
