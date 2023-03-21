4c485300842dd4ddeddb0a7860fd0f6f50db10af
vikz95 <vik.z.1995@gmail.com>
2023-03-21 21:32:41 +0100

    Changed example to use a C function available also on Windows

--- mockall/examples/ffi.rs
+++ mockall/examples/ffi.rs
@@ -1,35 +1,40 @@
 // vim: tw=80
 //! An example of unit testing involving mocked FFI functions
 
-#![cfg(unix)]
-
+use std::ptr;
 #[cfg(test)]
 use mockall::automock;
 use mockall_double::double;
+use std::time::{Duration, SystemTime};
 
 #[cfg_attr(test, automock)]
 pub mod mockable_ffi {
     extern "C" {
-        pub fn getuid() -> u32;
+        pub fn time(timer: *const u64) -> u64;
     }
 }
 
 #[double]
 use mockable_ffi as ffi;
 
-fn getuid() -> u32 {
-    unsafe { ffi::getuid() }
+fn time() -> u64 {
+    unsafe { ffi::time(ptr::null()) }
 }
 
 fn main() {
-    println!("My uid is {}", getuid() );
+    println!("Seconds since epoch: {}", time() );
 }
 
 
 #[test]
-fn getuid_test() {
-    let ctx = ffi::getuid_context();
+fn time_test() {
+    let seconds_from_epoch = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap_or(Duration::from_secs(0))
+        .as_secs();
+
+    let ctx = ffi::time_context();
     ctx.expect()
-        .return_const(42u32);
-    getuid();
+        .return_const(seconds_from_epoch);
+    time();
 }
