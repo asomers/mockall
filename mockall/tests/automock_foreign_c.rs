98198d6c5e0bab21bd40cfecdd0d5d1a24feba76
enes1313 <destansi.13@gmail.com>
2023-08-01 20:31:10 +0300

    Fixed race condition for foreign tests

--- mockall/tests/automock_foreign_c.rs
+++ mockall/tests/automock_foreign_c.rs
@@ -2,6 +2,7 @@
 #![deny(warnings)]
 
 use mockall::*;
+use std::sync::Mutex;
 
 #[automock]
 mod ffi {
@@ -12,6 +13,10 @@ mod ffi {
         #[allow(dead_code)]
         pub(super) fn foo1(x: u32) -> i64;
     }
+}
+
+lazy_static! {
+    static ref FOO_MTX: Mutex<()> = Mutex::new(());
 }
 
 // Ensure we can still use the original mocked function
@@ -25,25 +30,27 @@ pub fn normal_usage() {
 #[should_panic(expected = "mock_ffi::foo1(5): No matching expectation found")]
 fn with_no_matches() {
     let ctx = mock_ffi::foo1_context();
-    ctx.expect()
-        .with(predicate::eq(4))
-        .returning(i64::from);
-    unsafe{ mock_ffi::foo1(5) };
+    ctx.expect().with(predicate::eq(4)).returning(i64::from);
+    unsafe { mock_ffi::foo1(5) };
 }
 
 #[test]
 fn returning() {
+    let _m = FOO_MTX.lock().unwrap();
+
     let ctx = mock_ffi::foo_context();
     ctx.expect().returning(i64::from);
-    assert_eq!(42, unsafe{mock_ffi::foo(42)});
+    assert_eq!(42, unsafe { mock_ffi::foo(42) });
 }
 
 /// Ensure that the mock function can be called from C by casting it to a C
 /// function pointer.
 #[test]
 fn c_abi() {
+    let _m = FOO_MTX.lock().unwrap();
+
     let ctx = mock_ffi::foo_context();
     ctx.expect().returning(i64::from);
     let p: unsafe extern "C" fn(u32) -> i64 = mock_ffi::foo;
-    assert_eq!(42, unsafe{p(42)});
+    assert_eq!(42, unsafe { p(42) });
 }
