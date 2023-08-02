31709a14726f9fae38f1c619cdb174fee56f0044
Enes Aydın <destansi.13@gmail.com>
2023-08-02 22:40:42 +0300

    Added C variadic test (#508)
    Test that foreign C functions with variadic argument lists can be mocked.
    The variadic arguments won't be accessible to the matchers or returners, however.

---
+++ mockall/tests/automock_foreign_c_variadic.rs
@@ -0,0 +1,22 @@
+// vim: tw=80
+#![cfg_attr(feature = "nightly", feature(c_variadic))]
+#![deny(warnings)]
+
+#[cfg(feature = "nightly")]
+use mockall::*;
+
+#[automock]
+#[cfg(feature = "nightly")]
+pub mod ffi {
+    extern "C" {
+        pub fn foo(x: i32, y: i32, ...) -> i32;
+    }
+}
+
+#[test]
+#[cfg(feature = "nightly")]
+fn mocked_c_variadic() {
+    let ctx = mock_ffi::foo_context();
+    ctx.expect().returning(|x, y| x * y);
+    assert_eq!(6, unsafe{mock_ffi::foo(2, 3, 1, 4, 1)});
+}
