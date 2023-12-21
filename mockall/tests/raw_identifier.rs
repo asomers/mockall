asomers/mockall

commit 60acf8e41cb0497ca538e7d36337efaa338afa8e

Clippy cleanup: uninlined_format_args

mockall_derive/src/lib.rs
@@ -946,16 +946,16 @@ fn gen_keyid(g: &Generics) -> impl ToTokens {
 
 /// Generate a mock identifier from the regular one: eg "Foo" => "MockFoo"
 fn gen_mock_ident(ident: &Ident) -> Ident {
-    format_ident!("Mock{}", ident)
+    format_ident!("Mock{ident}")
 }
 
 /// Generate an identifier for the mock struct's private module: eg "Foo" =>
 /// "__mock_Foo"
 fn gen_mod_ident(struct_: &Ident, trait_: Option<&Ident>) -> Ident {
     if let Some(t) = trait_ {
-        format_ident!("__mock_{}_{}", struct_, t)
+        format_ident!("__mock_{struct_}_{t}")
     } else {
-        format_ident!("__mock_{}", struct_)
+        format_ident!("__mock_{struct_}")
     }
 }
 
@@ -1202,7 +1202,7 @@ fn mock_it<M: Into<MockableItem>>(inputs: M) -> TokenStream
     let mock = MockItem::from(mockable);
     let ts = mock.into_token_stream();
     if env::var("MOCKALL_DEBUG").is_ok() {
-        println!("{}", ts);
+        println!("{ts}");
     }
     ts
 }

mockall_derive/src/mock_function.rs
@@ -576,7 +576,7 @@ impl MockFunction {
             .doc(false)
             .format();
         let context_docstr = format!("Create a [`Context`]({}{}/struct.Context.html) for mocking the `{}` method",
-            modname.map(|m| format!("{}/", m)).unwrap_or_default(),
+            modname.map(|m| format!("{m}/")).unwrap_or_default(),
             self.inner_mod_ident(),
             self.name());
         let context_ident = format_ident!("{}_context", self.name());
@@ -602,7 +602,7 @@ impl MockFunction {
             format!("{}::{}", self.mod_ident, self.sig.ident)
         };
         let fields = vec!["{:?}"; argnames.len()].join(", ");
-        let fstr = format!("{}({})", name, fields);
+        let fstr = format!("{name}({fields})");
         quote!(std::format!(#fstr, #(::mockall::MaybeDebugger(&#argnames)),*))
     }
 
@@ -623,7 +623,7 @@ impl MockFunction {
             .doc(false)
             .format();
         let name = self.name();
-        let expect_ident = format_ident!("expect_{}", &name);
+        let expect_ident = format_ident!("expect_{name}");
         let expectation_obj = self.expectation_obj(self_args);
         let funcname = &self.sig.ident;
         let (_, tg, _) = if self.is_method_generic() {
@@ -649,7 +649,7 @@ impl MockFunction {
         let must_use = quote!();
 
         let substruct_obj = if let Some(trait_) = &self.trait_ {
-            let ident = format_ident!("{}_expectations", trait_);
+            let ident = format_ident!("{trait_}_expectations");
             quote!(#ident.)
         } else {
             quote!()
@@ -866,7 +866,7 @@ impl<'a> ToTokens for Common<'a> {
         let lg = lifetimes_to_generics(&self.f.alifetimes);
         let refpredty = &self.f.refpredty;
         let with_generics_idents = (0..self.f.predty.len())
-            .map(|i| format_ident!("MockallMatcher{}", i))
+            .map(|i| format_ident!("MockallMatcher{i}"))
             .collect::<Vec<_>>();
         let with_generics = with_generics_idents.iter()
             .zip(self.f.predty.iter())
@@ -1025,7 +1025,7 @@ impl<'a> ToTokens for CommonExpectationMethods<'a> {
         let lg = lifetimes_to_generics(&self.f.alifetimes);
         let predty = &self.f.predty;
         let with_generics_idents = (0..self.f.predty.len())
-            .map(|i| format_ident!("MockallMatcher{}", i))
+            .map(|i| format_ident!("MockallMatcher{i}"))
             .collect::<Vec<_>>();
         let with_generics = with_generics_idents.iter()
             .zip(self.f.predty.iter())
@@ -1212,7 +1212,7 @@ impl<'a> ToTokens for ExpectationGuardCommonMethods<'a> {
         let output = &self.f.output;
         let predty = &self.f.predty;
         let with_generics_idents = (0..self.f.predty.len())
-            .map(|i| format_ident!("MockallMatcher{}", i))
+            .map(|i| format_ident!("MockallMatcher{i}"))
             .collect::<Vec<_>>();
         let with_generics = with_generics_idents.iter()
             .zip(self.f.predty.iter())

mockall/src/lib.rs
@@ -1668,7 +1668,7 @@ impl SeqInner {
     /// Verify that the call identified by `seq` was called in the correct order
     fn verify(&self, seq: usize, desc: &str) {
         assert_eq!(seq, self.satisfaction_level.load(Ordering::Relaxed),
-            "{}: Method sequence violation", desc)
+            "{desc}: Method sequence violation")
     }
 }
 

mockall_double/src/lib.rs
@@ -180,7 +180,7 @@ fn mock_itemuse(orig: &mut ItemUse) {
 }
 
 fn mock_ident(i: &Ident) -> Ident {
-    let is_type = format!("{}", i)
+    let is_type = format!("{i}")
         .chars()
         .next()
         .expect("zero-length ident?")

mockall/tests/automock_debug.rs
@@ -19,17 +19,17 @@ impl Bean{}
 #[test]
 fn automock_trait() {
     let foo = MockFoo::new();
-    assert_eq!("MockFoo", format!("{:?}", foo));
+    assert_eq!("MockFoo", format!("{foo:?}"));
 }
 
 #[test]
 fn automock_struct_impl() {
     let bean = MockBean::new();
-    assert_eq!("MockBean", format!("{:?}", bean));
+    assert_eq!("MockBean", format!("{bean:?}"));
 }
 
 #[test]
 fn automock_trait_impl() {
     let baz = MockBaz::new();
-    assert_eq!("MockBaz", format!("{:?}", baz));
+    assert_eq!("MockBaz", format!("{baz:?}"));
 }

mockall/tests/mock_debug.rs
@@ -30,11 +30,11 @@ impl Debug for MockBaz {
 #[test]
 fn automatic() {
     let bar = MockBar::new();
-    assert_eq!("MockBar", format!("{:?}", bar));
+    assert_eq!("MockBar", format!("{bar:?}"));
 }
 
 #[test]
 fn manual() {
     let baz = MockBaz::new();
-    assert_eq!("XXX", format!("{:?}", baz));
+    assert_eq!("XXX", format!("{baz:?}"));
 }

mockall_derive/src/mock_item.rs
@@ -165,7 +165,7 @@ impl ToTokens for MockItemModule {
         ).to_tokens(&mut body);
         let docstr = {
             if let Some(ident) = &self.orig_ident {
-                let inner = format!("Mock version of the `{}` module", ident);
+                let inner = format!("Mock version of the `{ident}` module");
                 quote!( #[doc = #inner])
             } else {
                 // Typically an extern FFI block.  Not really anything good we

mockall_derive/src/mock_item_struct.rs
@@ -14,7 +14,7 @@ fn phantom_default_inits(generics: &Generics) -> Vec<TokenStream> {
     .iter()
     .enumerate()
     .map(|(count, _param)| {
-        let phident = format_ident!("_t{}", count);
+        let phident = format_ident!("_t{count}");
         quote!(#phident: ::std::marker::PhantomData)
     }).collect()
 }
@@ -25,7 +25,7 @@ fn phantom_fields(generics: &Generics) -> Vec<TokenStream> {
     .iter()
     .enumerate()
     .filter_map(|(count, param)| {
-        let phident = format_ident!("_t{}", count);
+        let phident = format_ident!("_t{count}");
         match param {
             syn::GenericParam::Lifetime(l) => {
                 if !l.bounds.is_empty() {

commit 71823a3acc31a11a45e5b28f7612fb5ed88053af

Fix mocking functions that use raw identifiers for their names.
This was a regression in 0.12.0, introduced by 60acf8e .

Fixes #533

mockall_derive/src/lib.rs
@@ -970,14 +970,14 @@ fn gen_keyid(g: &Generics) -> impl ToTokens {
 
 /// Generate a mock identifier from the regular one: eg "Foo" => "MockFoo"
 fn gen_mock_ident(ident: &Ident) -> Ident {
-    format_ident!("Mock{ident}")
+    format_ident!("Mock{}", ident)
 }
 
 /// Generate an identifier for the mock struct's private module: eg "Foo" =>
 /// "__mock_Foo"
 fn gen_mod_ident(struct_: &Ident, trait_: Option<&Ident>) -> Ident {
     if let Some(t) = trait_ {
-        format_ident!("__mock_{struct_}_{t}")
+        format_ident!("__mock_{struct_}_{}", t)
     } else {
         format_ident!("__mock_{struct_}")
     }

mockall_derive/src/mock_function.rs
@@ -629,7 +629,7 @@ impl MockFunction {
             .doc(false)
             .format();
         let name = self.name();
-        let expect_ident = format_ident!("expect_{name}");
+        let expect_ident = format_ident!("expect_{}", name);
         let expectation_obj = self.expectation_obj(self_args);
         let funcname = &self.sig.ident;
         let (_, tg, _) = if self.is_method_generic() {

mockall/tests/raw_identifier.rs
@@ -1,0 +1,72 @@
+// vim: tw=80
+//! It should be possible to mock things that use raw identifiers
+#![deny(warnings)]
+#![allow(non_camel_case_types)]
+
+use mockall::*;
+
+#[automock]
+trait r#while {
+    fn r#match(&self);
+    fn r#loop();
+}
+
+#[automock]
+pub mod r#break {
+    pub fn r#if() {unimplemented!() }
+}
+
+mock! {
+    r#do {}
+    impl r#while for r#do {
+        fn r#match(&self);
+        fn r#loop();
+    }
+}
+
+struct r#else {}
+#[automock]
+impl r#while for r#else {
+    fn r#match(&self) {unimplemented!()}
+    fn r#loop() {unimplemented!()}
+}
+
+#[test]
+fn by_ref() {
+    let mut foo = Mockwhile::new();
+    foo.expect_match()
+        .return_const(());
+    foo.r#match();
+}
+
+#[test]
+fn static_method() {
+    let ctx = Mockwhile::loop_context();
+    ctx.expect()
+        .returning(|| ());
+    Mockwhile::r#loop();
+}
+
+#[test]
+fn manual_mock() {
+    let mut foo = Mockdo::new();
+    foo.expect_match()
+        .return_const(());
+    foo.r#match();
+}
+
+#[test]
+fn module() {
+    let ctx = mock_break::if_context();
+    ctx.expect()
+        .returning(|| ());
+    mock_break::r#if();
+}
+
+#[test]
+fn trait_impl() {
+    let mut mock = Mockelse::new();
+    mock.expect_match()
+        .returning(|| ());
+    mock.r#match();
+}